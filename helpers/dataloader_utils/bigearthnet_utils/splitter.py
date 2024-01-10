949ecbdd81bf64f3f8c54187c3d393a2bc120e6e
lionelblonde <lionel.blonde@me.com>
2024-01-11 00:58:53 +0100

    Correct erroneous typing

--- helpers/dataloader_utils/bigearthnet_utils/splitter.py
+++ helpers/dataloader_utils/bigearthnet_utils/splitter.py
@@ -1,16 +1,15 @@
 from pathlib import Path
-from typing import Dict
 
 from helpers.dataloader_utils import path2str
 
 
-def split_bigearthnet_official(num_classes: int) -> Dict[str, str]:
+def split_bigearthnet_official(num_classes: int) -> list[str]:
     where_splits = (f"./splits/BigEarthNet-S2_{num_classes}-classes_OFFICIAL/splits")
     txt_files = ['train.txt', 'val.txt', 'test.txt']
     return [path2str(Path(where_splits).joinpath(f)) for f in txt_files]
 
 
-def split_dataset(dataset_handle: str, num_classes: int) -> Dict[str, str]:
+def split_dataset(dataset_handle: str, num_classes: int) -> list[str]:
     paths_list = []
 
     if dataset_handle == "bigearthnet":
